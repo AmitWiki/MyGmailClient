e1466874c1c3ff472f3dd6b1e1bb205e0b4f20e0
slackeramit <amit520433@gmail.com>
2015-11-04 22:57:06 +0530

    Initial Commit

---
+++ MyGmailApp/App_Data/DatabaseObjects.sql
@@ -0,0 +1,29 @@
+﻿CREATE TABLE [dbo].[UserRoles]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[UserRole] varchar(100) not null PRIMARY KEY,
+	[RoleDescription] varchar(100) not null
+)
+
+CREATE TABLE [dbo].[Logins]
+(
+	[Id] INT identity(1,1) NOT NULL,
+	[Username] varchar(100) NOT NULL PRIMARY KEY,
+	[Password]  varchar(100) NOT NULL,
+	[UserRole]  varchar(100) NOT NULL,
+	[Active]  varchar(100) NOT NULL
+)
+
+
+CREATE TABLE [dbo].[GmailConfig]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Username] varchar(100) foreign key references Logins,
+	[GmailUsername] varchar(100) not null,
+	[GmailPassword] varchar(100) not null,
+	[IncomingServerAddress] varchar(100) not null,
+	[OutgoingServerAddress] varchar(100) not null,
+	[IncomingServerPort] varchar(100) not null,
+	[OutgoingServerPort] varchar(100) not null,
+	[UseSSL] bit not null
+)
